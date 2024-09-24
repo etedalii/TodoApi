f782075b59e7749ab52e12fd1ec57e5c91ac3c3b
etedali <etedali@gmail.com>
2024-09-24 16:53:30 -0400

    added getbyId

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -7,7 +7,7 @@ namespace TodoApi.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class ProductController : ControllerBase 
+public class ProductController : ControllerBase
 {
     [HttpGet]
     public IActionResult Get()
@@ -20,8 +20,30 @@ public class ProductController : ControllerBase
             .RuleFor(p => p.ExpireDate, f => f.Date.Future(2))  // Expires within the next 2 years
             .RuleFor(p => p.Inventory, f => f.Random.Int(0, 100));  // Random inventory between 0 and 100
 
-        List<Product> products = faker.Generate(new Random().Next(5,100));
-        
+        List<Product> products = faker.Generate(new Random().Next(5, 100));
+
         return Ok(products);
+    }
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        if (id > 5000)
+            id = 5000;
+        if (id < 1)
+            id = 1;
+
+        var faker = new Faker<Product>()
+            // Generate fake data for each property
+            .RuleFor(p => p.Id, f => f.IndexFaker + 1)
+            .RuleFor(p => p.Name, f => f.Commerce.ProductName())
+            .RuleFor(p => p.Price, f => float.Parse(f.Commerce.Price(min: 7, max: 1045340)))
+            .RuleFor(p => p.ExpireDate, f => f.Date.Future(2))  // Expires within the next 2 years
+            .RuleFor(p => p.Inventory, f => f.Random.Int(0, 100));  // Random inventory between 0 and 100
+
+        List<Product> products = faker.Generate(5000);
+        var product = products.Where(_ => _.Id == id);
+        if (product == null)
+            return NotFound();
+        return Ok(product);
     }
 }
--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4be75e5bbacd9e85a2bf95263c558b5922c1ef93")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
