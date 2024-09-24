f1cd7f8a5332601b6da60d31f7994a4480543f09
etedali <etedali@gmail.com>
2024-09-24 17:08:21 -0400

    A

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,10 +5,9 @@ namespace TodoApi.Models;
 public class Product
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public float Price { get; set; }
     public DateTime ExpireDate { get; set; }
 
     public int Inventory { get; set; }
-    
 }
